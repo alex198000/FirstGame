fb83316db6ff463dfe8dd31f14d478e61816e79d
alex198000 <85245556+alex198000@users.noreply.github.com>
2021-06-08 20:16:32 +0300

    sotka

--- FirstProject/Assets/Scripts/Menu.cs
+++ FirstProject/Assets/Scripts/Menu.cs
@@ -10,10 +10,7 @@ public class Menu : MonoBehaviour {
 
     private void Start()
     {
-        if (pLost.GetComponent<AudioSource>().mute == true)
-            PlayerPrefs.SetInt("SoundOff", 1);
-        else
-                PlayerPrefs.SetInt("SoundOff", 0);
+        
     }
 
     void OnGUI()
@@ -80,7 +77,10 @@ public class Menu : MonoBehaviour {
         if (GUI.Button(new Rect(Screen.width - 250, 250, 200, 55), "Music"))
         {
             pLost.GetComponent<AudioSource>().mute = !pLost.GetComponent<AudioSource>().mute;
-            PlayerPrefs.GetInt("SoundOff", 1);
+            if (pLost.GetComponent<AudioSource>().mute == true)
+                PlayerPrefs.SetInt("SoundOff", 1);
+            else
+                PlayerPrefs.SetInt("SoundOff", 0);
         }
         if (pLost.GetComponent<AudioSource>().mute == true)
         {
---
+++ FirstProject/Assets/Scripts/Sotka.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sotka : MonoBehaviour
+{
+    [SerializeField] private int length = 50;
+    [SerializeField] private int width = 50;
+    [SerializeField] private int reference = 20;
+    [SerializeField] private int sotka;
+    [SerializeField] private int metrKvadr;
+    [SerializeField] private int price = 80;
+    [SerializeField] private int sotka1;
+    [SerializeField] private int sotka2;
+    [SerializeField] private int sotka3;
+
+    void Start()
+    {
+        
+        sotka = (length * width) / 100;
+        Purchase(length, width, reference);
+        metrKvadr = price / sotka;
+        BestKvadr(sotka, price);
+        Optima(sotka1 = 60, sotka2 = 50, sotka3 = 40);
+    }
+
+    // Update is called once per frame
+    private void Purchase(int length, int width, int reference)
+    {
+        if (sotka >= reference)
+        {
+            print("Покупаем по цене " + sotka * price);
+           
+        }
+
+
+        else
+        {
+            print("не покупаем");
+        }
+    }
+    private void BestKvadr(int sotka, int price)
+    {
+        metrKvadr = price / sotka;
+        print("1 кв метр выходит по цене " + metrKvadr);
+    }
+
+    private void Optima(int sotka1, int sotka2, int sotka3)
+    {
+        if (sotka1 > sotka2)
+        {
+            if (sotka1 > sotka3)
+            {
+                print("Самая большая сотка №1 " + sotka1);
+            }
+            else if (sotka2 > sotka3)
+            {
+                print("Самая большая сотка №2 " + sotka2);
+            }
+            
+        }
+        else
+        {
+            print("Самая большая сотка №3 " + sotka3);
+        }
+    }
+       
+
+}
+
+
